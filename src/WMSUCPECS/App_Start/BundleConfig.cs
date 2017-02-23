e2387fd2c52ff525b9649db81fb0b86e887efe5f
awaing-wmsu <waing.alex@gmail.com>
2017-02-23 08:09:01 +0800

    -Tried JQuery DataTables, Bootstrap Modals = Unsuccessful.

--- src/WMSUCPECS/App_Start/BundleConfig.cs
+++ src/WMSUCPECS/App_Start/BundleConfig.cs
@@ -27,6 +27,8 @@ namespace WMSUCPECS
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
+            bundles.Add(new ScriptBundle("~/bundles/modalform").Include("~/Scripts/modalform.js"));
+
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
             BundleTable.EnableOptimizations = true;
